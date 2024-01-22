721154f40bf9a624b2779d70df850aa7c93fdbf0
bruceg14 <ziyuan.guo@vanderbilt.edu>
2024-01-21 23:52:44 -0500

    For depoloy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React, {useState} from 'react';
 import './App.css';
-import LinkedIn from './img/LinkedIn-Icon.png'
-import GitHub from './img/GitHub-Icon.png'
-import Email_Icon from './img/Email-Icon.png'
 import Vanderbilt_Picture from './img/Vandy_Icon.jpeg'
-import JavaScript_Icon from './img/Javascript_Icon.png'
-import Node_JS_Icon from './img/node_js.png'
-import CPlusPlus_Icon from './img/C++_Icon.jpeg'
-import Python_Icon from './img/Python_Icon.png'
-import R_Icon from './img/R_Icon.png'
-import Java_Icon from './img/Java_Icon.svg'
-import React_Icon from './img/React-Icon.png'
-import React_Native from './img/React-Native_Icon.png'
-import MongoDB_Icon from './img/MongoDB_Icon.png'
-import Express_Icon from './img/Express_Icon.svg'
-import AWS_Icon from './img/AWS_Icon.jpeg'
-import Git_Icon from './img/Git_Icon.png'
-import SQL_Icon from './img/SQL_Icon.png'
-import Linux_Icon from './img/Linux_Icon.jpeg'
-import Tailwind_Icon from './img/TailwindCSS_Icon.jpeg'
-import Django_Icon from './img/Django_Icon.png'
-
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import {Button, ButtonGroup, Box, Typography, }  from '@mui/material';
 
 const Education = () => {
@@ -42,11 +25,6 @@ const Education = () => {
 }
 
 const SkillSet = () => {
-  const languages = ["Javascript", "Node.js", "C++", "Python", "R", "Java"]
-  const languages_icons =[JavaScript_Icon, Node_JS_Icon, CPlusPlus_Icon, Python_Icon, R_Icon, Java_Icon]
-  
-  const skillSet_icons=[React_Icon, React_Native, MongoDB_Icon, Express_Icon, Tailwind_Icon, AWS_Icon, 
-    Git_Icon, SQL_Icon, Linux_Icon, Django_Icon]
   return (
     <div className="flex justify-start hover:bg-neutral-100 bg-white rounded-lg">
       <div className=" p-8">
@@ -63,27 +41,7 @@ const SkillSet = () => {
           React, Next.js, ASP .NET, Firebase, MongoDB, AWS, Azure, Google Cloud Platform, REST API, CI/CD
         </p>
       </div>
-      {/* <div >
-        <h1 className="p-6 font-extrabold text-gray-700 text-4xl">Languages</h1>
-        <ul role="list" className="p-6 max-h-200 overflow-y-auto">
-          {languages_icons.map((item, index) => {
-            return(
-              <li className= "bg-white ring ring-neutral-300  w-20 h-20 mb-4" key={index}><img src={item} alt={languages[index]} /></li>
-            )
-          })}
-        </ul>
-      </div>
-
-      <div>
-        <h1 className="p-6 font-extrabold text-gray-700 text-4xl">Skill Set</h1>
-        <ul role="list" className="p-6 max-h-200 overflow-y-auto">
-          {skillSet_icons.map((item, index) => {
-            return(
-              <li className='bg-white ring ring-neutral-300 w-20 h-20 mb-4' key={index}><img src={item} alt=""/></li>
-            )
-          })}
-        </ul>
-      </div> */}
+      
     </div>
   )
 }
@@ -96,13 +54,14 @@ const WorkExperience = () => {
       title: 'Undergraduate Researcher, HIDe Lab at Vanderbilt University',
       date: 'May 2023 - December 2023',
       content: 
-        'Researched with Prof. Dana Zhang for developing an interoperable and efficient identity system using distributed ledger technologies. \nDesigned and implemented a mobile decentralized application using React-native that allow user to store and share their information using Hyperledger technology'  },
+        `Developed an interoperable mobile application using Hyperledger Fabric blockchain technology and React-Native for rare-diseased patients.
+         \n Researched and published a paper on the application of Hyperledger Fabric technology on permission-based healthcare data management system that was accepted by IEEE BIBM 2023.`  },
     {
       id: 2,
       title: 'Mobile Application Development Intern at Give Black',
       date: 'February 2023 - May 2023',
       content:
-        'Update the UI of the mobile application for iOS system using XCode, and managed the SQL database of user information through Bluehost' 
+        'Update the UI of the Give Black mobile application for iOS system using XCode. \nManaged and updated the SQL database of user information through Bluehost.' 
     }
     
   ];
@@ -132,40 +91,79 @@ const WorkExperience = () => {
 const PersonalProject = () => {
   return (
     <div className="p-8">
-      <div className="max-w-3xl mx-auto">
-        <h1 className="text-4xl font-bold mb-4">Wok Finder</h1>
-        <p className="text-gray-600 mb-6">
-          This project apply React-Native to build a cross-platform mobile application that highlight Asian restaurant
-          in Nashville. This is an on-going project since June 2023. 
-        </p>
+      <ul>
+        <li className="border border-gray-300 p-4 rounded hover:bg-gray-100 ">
+        <div className="max-w-3xl mx-auto">
+            <h1 className="text-4xl font-bold mb-4">Duckling</h1>
+            <p className="text-gray-600 mb-6">
+              This project apply Next.js for front-end development and Express.js for back-end API. It is an social media web application
+              that is designed for people to showcase their fashion choice and let people choose their outfit base on others rating.
+              It is deployed on Vercel and it is still a work in progress. 
+            </p>
 
-        <div className="relative w-full h-0" style={{ paddingBottom: '56.25%' }}>
-          <iframe
-            className="absolute top-0 left-0 w-full h-full"
-            src="https://www.youtube.com/embed/79G3GIm39-E"
-            title="Wok-Finder Video"
-            allowFullScreen
-          />
-        </div>
+            {/* <div className="relative w-full h-0" style={{ paddingBottom: '56.25%' }}>
+              <iframe
+                className="absolute top-0 left-0 w-full h-full"
+                src="https://www.youtube.com/embed/79G3GIm39-E"
+                title="Wok-Finder Video"
+                allowFullScreen
+              />
+            </div>
 
-        <div className="mt-6">
-          <a
-            href="https://github.com/Food-App-Dev-Team/Food_App_React-native"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300"
-          >
-            View Project Repository
-          </a>
-        </div>
+            <div className="mt-6">
+              <a
+                href="https://github.com/Food-App-Dev-Team/Food_App_React-native"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300"
+              >
+                View Project Repository
+              </a>
+            </div> */}
 
-        <h2 className="text-2xl font-bold mt-8">About</h2>
-        <p className="mt-2">
-          All the functionality, user interface, backend API call and database set up occured in the video are designed and wrote by me. 
-        </p>
+            {/* <h2 className="text-2xl font-bold mt-8">About</h2>
+            <p className="mt-2">
+              All the functionality, user interface, backend API call and database set up occured in the video are designed and wrote by me. 
+            </p> */}
+          </div>
+        </li>
+        <li className="border border-gray-300 p-4 rounded hover:bg-gray-100 mt-5">
+          <div className="max-w-3xl mx-auto">
+            <h1 className="text-4xl font-bold mb-4">Wok Finder</h1>
+            <p className="text-gray-600 mb-6">
+              This project apply React-Native to build a cross-platform mobile application that highlight Asian restaurant
+              in Nashville. 
+            </p>
 
-        {/* Add more content about your project as needed */}
-      </div>
+            <div className="relative w-full h-0" style={{ paddingBottom: '56.25%' }}>
+              <iframe
+                className="absolute top-0 left-0 w-full h-full"
+                src="https://www.youtube.com/embed/79G3GIm39-E"
+                title="Wok-Finder Video"
+                allowFullScreen
+              />
+            </div>
+
+            <div className="mt-6">
+              <a
+                href="https://github.com/Food-App-Dev-Team/Food_App_React-native"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300"
+              >
+                View Project Repository
+              </a>
+            </div>
+
+            <h2 className="text-2xl font-bold mt-8">About</h2>
+            <p className="mt-2">
+              All the functionality, user interface, backend API call and database set up occured in the video are designed and wrote by me. 
+            </p>
+
+            {/* Add more content about your project as needed */}
+          </div>
+        </li>
+      </ul>
     </div>
   );
 
@@ -225,42 +223,40 @@ function App() {
 
   return (
     
-    <div className="w-screen h-800 bg-neutral-200 leading-relaxed selection:bg-blue-300 antialiased ">
-      <div className="bg-slate-200 pt-2 pb-4 ">
+    <div className="w-screen bg-neutral-200 leading-relaxed selection:bg-blue-300 antialiased overflow-y-auto p-2">
+      <div className="bg-blue-300 pt-2 pb-4 fixed top-0 left-0 z-10 h-[20vh] w-[100%]">
         <h1 className=" font-serif items-center justify-center flex text-5xl m-2 " >Bruce Guo</h1>
-        <div className="flex flex-row pb-4">
-          <div class="basis-1/3 items-center justify-center flex ">
-            <a
-              href="https://www.linkedin.com/in/ziyuan-guo-bruce/"
-              className="text-black-500 hover:text-blue-700 items-center justify-center flex "
-              >
-              {/* <p className="font-mono">LinkedIn Profile</p> */}
-              <img src={LinkedIn} alt="LinkedIn Profile" className="w-1/6 h-1/6 hover:ring-4 hover:ring-blue-700 rounded-full ring-1" />
-            </a>
-          </div>
+        <div className="flex flex-row pb-4 mt-4">
           <div className="basis-1/3 ">
             <a
               href="https://github.com/bruceg14"
               className="text-black-500 hover:text-blue-700 items-center justify-center flex"
               >
-              {/* <p className="font-mono">Github Profile</p> */}
-              <img src={GitHub} alt="GitHub Profile" className="w-1/6 h-1/6 hover:ring-4 hover:ring-blue-700 rounded-full ring-1" />
+              <GitHubIcon sx={{fontSize: '50px'}}/> 
             </a>
           </div>
+          <div class="basis-1/3 items-center justify-center flex ">
+            <a
+              href="https://www.linkedin.com/in/ziyuan-guo-bruce/"
+              className="text-black-500 hover:text-blue-700 items-center justify-center flex "
+              >
+              <LinkedInIcon sx={{fontSize: '50px'}}/>
+            </a>
+          </div>
+          
           <div className="basis-1/3 ">
             <a href="mailto:ziyuan.guo@vanderbilt.edu"
             className="text-black-500 hover:text-blue-700 items-center justify-center flex"
 
             >
-              <img src={Email_Icon} alt="ziyuan.guo@vanderbilt.edu" className="w-1/6 h-1/6 hover:ring-4 hover:ring-blue-700 ring-1 rounded-full" />
-              {/* <p className="font-mono">ziyuan.guo@vanderbilt.edu</p> */}
+              <EmailIcon sx={{fontSize: '50px'}}/> 
             </a>
           </div>
         </div>
       </div>
 
       <div className="flex border-t-2 border-black">
-        <div className="w-1/3 h-screen">
+        <div className="w-1/3 h-screen fixed top-[20%] z-6">
           <div className="h-full flex flex-col justify-start items-start p-4">
             <Box sx={{marginTop: "10%", }}>
               <Button className=" w-3/4 mb-4 mt-5" onClick={handlePersonal}>
@@ -281,9 +277,8 @@ function App() {
           
           </div>
         </div>
-        <div className="w-2/3 h-full">
-          <div className="h-full flex-1  p-4">
-         
+        <div className=" h-full  ml-[30%] mt-[20vh] z-0 min-h-[80vh]">
+          <div className="h-full p-4">
           {education ? <div className=""><Education className="h-full"/></div> : <div></div>}
           {skillSet ? <div className=""><SkillSet className="h-full"/></div> : <div></div>}
           {workExperience ? <div className=""><WorkExperience className="h-full"/></div> : <div></div>}
